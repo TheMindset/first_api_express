9aeb380fb1ce5267a322c8f26a563aa8aa8d2923
TheMindset <mulezig2014@outlook.fr>
2019-12-08 16:42:19 +0100

    add a test database

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,18 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/publications',
+    connection: 'postgres://localhost/publications_dev',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }, 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/publications_test',
     migrations: {
       directory: './db/migrations'
     },
